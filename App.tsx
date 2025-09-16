89b3cfb8994ec4419758248497700e7d0c4a10ea
daeseopk <eotjqdlee@gmail.com>
2025-09-16 13:58:39 +0900

    add eslint

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import "react-native-gesture-handler";
 import { enableScreens } from "react-native-screens";
 import AppNavigator from "./src/navigation/AppNavigator";
+
 enableScreens();
 
 export default function App() {
