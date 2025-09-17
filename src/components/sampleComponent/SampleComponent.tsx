823fe37b43658e4fe334cde22fc8899017dff8e6
daeseopk <eotjqdlee@gmail.com>
2025-09-17 17:52:19 +0900

    add sample structure

---
+++ src/components/sampleComponent/SampleComponent.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function SampleComponent() {
+  return <View></View>;
+}
---
+++ src/views/sampleView/SampleView.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function SampleView() {
+  return <View></View>;
+}
